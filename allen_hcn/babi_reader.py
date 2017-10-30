6f491ec65c7bfd20a900de8b25d7a53bdedcc368
Olga Gureenkova <ol.gure@gmail.com>
2017-10-30 18:16:02 +0700

    refactor: entity tracker and action tracker are no more the parts of BabiDatasetReader() constructor

--- allen_hcn/babi_reader.py
+++ allen_hcn/babi_reader.py
@@ -30,10 +30,8 @@ class BabiDatasetReader(DatasetReader):
         target_tokens: ``TextField``
     """
 
-    def __init__(self, entity_tracker: HCNEntityTracker, action_tracker: HCNActionTracker,
+    def __init__(self,
                  token_indexers: Dict[str, TokenIndexer] = None):
-        self._action_templates = SequenceField(action_tracker.get_action_templates())
-        self._et = entity_tracker
         self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
         self.dialog_indices = None
 
@@ -42,6 +40,12 @@ class BabiDatasetReader(DatasetReader):
         """
         Read data from the `file_path` and return a :class:`Dataset`.
         """
+        # set trackers
+        et = HCNEntityTracker()
+        at = HCNActionTracker(et, file_path)
+
+        action_templates = at.action_templates()
+
         # get dialogs from file
         logger.info("Reading instances from lines in file at: {}".format(file_path))
         dialogs, dialog_indices = util.read_dialogs(file_path, with_indices=True)
@@ -51,22 +55,23 @@ class BabiDatasetReader(DatasetReader):
         utterances = util.get_utterances(file_path, dialogs)
         # get responses
         responses = util.get_responses(file_path, dialogs)
-        responses = [self.get_template_id(response) for response in responses]
+        responses = [self.get_template_id(response, et, action_templates) for response in responses]
 
         instances = []
         for u, r in zip(utterances, responses):
-            instances.append(self.text_to_instance(u, r))
+            instances.append(self.text_to_instance(u, r, action_templates))
 
         if not instances:
             raise ConfigurationError("No instances read!")
 
         return Dataset(instances)
 
-    def text_to_instance(self, user_utterance: str, response_template: int = -1) -> Instance:
+    def text_to_instance(self, action_templates, user_utterance: str,
+                         response_template: int = -1) -> Instance:
         tokenized_source = user_utterance.split(' ')
         source_field = TextField(tokenized_source, self._token_indexers)
         if response_template != -1:
-            target_field = IndexField(response_template, self._action_templates)
+            target_field = IndexField(response_template, action_templates)
             return Instance({"source_tokens": source_field, "target_template_number": target_field})
         else:
             return Instance({'source_tokens': source_field})
@@ -76,28 +81,16 @@ class BabiDatasetReader(DatasetReader):
         """
         Constructs the dataset reader described by ``params``.
         """
-        action_tracker_type = params.pop('action_tracker', None)
-        entity_tracker_type = params.pop('entity_tracker', None)
-
-        if entity_tracker_type is None:
-            entity_tracker = None
-        else:
-            entity_tracker = HCNEntityTracker()
-
-        if action_tracker_type is None:
-            action_tracker = None
-        else:
-            action_tracker = HCNActionTracker(entity_tracker)
-
         token_indexers_type = params.pop('token_indexers', None)
         if token_indexers_type is None:
             token_indexers = None
         else:
             token_indexers = TokenIndexer.dict_from_params(token_indexers_type)
         params.assert_empty(cls.__name__)
-        return BabiDatasetReader(entity_tracker, action_tracker, token_indexers)
+        return BabiDatasetReader(token_indexers)
 
-    def get_template_id(self, response):
+    @staticmethod
+    def get_template_id(response, et, action_templates):
 
         def extract_(response):
             template = []
@@ -113,6 +106,4 @@ class BabiDatasetReader(DatasetReader):
                     template.append(word)
             return ' '.join(template)
 
-        return self.action_templates.index(
-            extract_(self.et.extract_entities(response, update=False))
-        )
+        return action_templates.index(extract_(et.extract_entities(response, update=False)))
